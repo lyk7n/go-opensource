fc300a585cb62931f3afd72b69379d41c38e4fd9
lyk7n <99960323+lyk7n@users.noreply.github.com>
2022-03-06 20:49:30 +0530

    Add files via upload

---
+++ bandwidthmonitor.py
@@ -0,0 +1,39 @@
+import time
+from traceback import print_tb
+import psutil
+
+
+last_recieved = psutil.net_io_counters().bytes_recv
+last_sent = psutil.net_io_counters().bytes_sent
+
+total = last_recieved + last_sent
+
+
+while True:
+      try:
+         bytes_recieved = psutil.net_io_counters().bytes_recv
+         bytes_sent = psutil.net_io_counters().bytes_sent
+
+         total_bytes = bytes_recieved + bytes_sent
+
+         new_recv = bytes_recieved - last_recieved
+         new_sent = bytes_sent - last_sent
+         new_total = total_bytes - total
+      
+      # this programs return the bandwidth in bytes so we convert bytes into megabytes 
+
+         receieved_in_mb = new_recv /1024 / 1024
+         sent_in_mb = new_sent /1024 /1024
+         total_in_mb = receieved_in_mb + sent_in_mb
+         
+         print(f"{receieved_in_mb:.2f}MB recieved, {sent_in_mb:.2f} MB sent, {total_in_mb:.2f} Total")
+         
+         last_recieved = bytes_recieved
+         last_sent = bytes_sent
+         total_bytes = total
+   
+
+
+         time.sleep(2)
+      except KeyboardInterrupt:
+            quit()
